bea006f43739386b5db644c113966059c3dd6dc7
naterrie <naterrie@student.42lyon.fr>
2025-11-27 10:42:39 +0100

    Refactor woody64 function for improved readability and maintainability; remove unused memory allocation code.

--- srcs/parsing/woody.c
+++ srcs/parsing/woody.c
@@ -3,12 +3,15 @@
 int	woody64(t_woody	*woody)
 {
 	(void)woody;
-	Elf64_Ehdr *elf_header = (Elf64_Ehdr *)woody->map;
-	Elf64_Phdr *program_header = (Elf64_Phdr *)(woody->map + elf_header->e_phoff);
-	Elf64_Shdr *section_header = (Elf64_Shdr *)(woody->map + elf_header->e_shoff);
-	Elf64_Shdr *sh_strtab = &section_header[elf_header->e_shstrndx];
-	const char  *sh_strtab_p = woody->map + sh_strtab->sh_offset;
-	Elf64_Shdr *text_sh = NULL;
+	Elf64_Ehdr	*elf_header = (Elf64_Ehdr *)woody->map;
+	Elf64_Phdr	*program_header = (Elf64_Phdr *)(woody->map + elf_header->e_phoff);
+	Elf64_Phdr	*last_phdr = NULL;
+	Elf64_Addr	last_segment_end = 0;
+
+	Elf64_Shdr	*section_header = (Elf64_Shdr *)(woody->map + elf_header->e_shoff);
+	Elf64_Shdr	*sh_strtab = &section_header[elf_header->e_shstrndx];
+	const char	*sh_strtab_p = woody->map + sh_strtab->sh_offset;
+	Elf64_Shdr	*text_sh = NULL;
 
 	for (int i = 0; i < elf_header->e_shnum; i++)
 	{
@@ -26,22 +29,17 @@ int	woody64(t_woody	*woody)
 		return (EXIT_FAILURE);
 	}
 
+
 	for (int i = 0; i < elf_header->e_phnum; i++)
 	{
 		if (program_header[i].p_type == PT_LOAD)
 		{
-			void *segments = mmap(NULL,
-				program_header[i].p_memsz,
-				PROT_READ | PROT_WRITE | PROT_EXEC,
-				MAP_PRIVATE | 0x20, //  is 0x20, but school env does not define it
-				-1,
-				0);
-			if (segments == MAP_FAILED)
+			Elf64_Addr segment_end = program_header[i].p_vaddr + program_header[i].p_memsz;
+			if (segment_end > last_segment_end)
 			{
-				dprintf(2, "Memory allocation for segment failed\n");
-				return (EXIT_FAILURE);
+				last_segment_end = segment_end;
+				last_phdr = &program_header[i];
 			}
-			memcpy(segments, woody->map + program_header[i].p_offset, program_header[i].p_filesz);
 		}
 	}
 	void *entry_point = (void *)(elf_header->e_entry);
@@ -49,7 +47,8 @@ int	woody64(t_woody	*woody)
 	printf(".text found: offset=0x%lx, addr=0x%lx, size=0x%lx\n",
 		(unsigned long)text_sh->sh_offset, (unsigned long)text_sh->sh_addr, (unsigned long)text_sh->sh_size);
 	printf("Entry point at address: %p\n", entry_point);
-
+	printf("Last PT_LOAD segment ends at address: 0x%lx\n", (unsigned long)last_segment_end);
+	printf("Last program header PT_LOAD p_vaddr: 0x%lx\n\n", (unsigned long)last_phdr->p_vaddr);
 	munmap(woody->map, woody->size);
 	return (EXIT_SUCCESS);
 }
